68d193f1b699a6dfcc1c970bd5321b72264fda7b
109B29689 <91858061+109B29689@users.noreply.github.com>
2021-12-20 13:28:54 +0800

    Update J3.java

--- J3.java
+++ J3.java
@@ -1,31 +1,4 @@
-interface IArea{
-    void area();
-}
-class Circle implements IArea{
-    private double r;
-    public Circle(double r){
-        this.r=r;
-    }
-    public void area(){
-        System.out.println("圓形面積:"+3.1416*r*r);
-    }
-}
-class  Rectangle implements IArea{
-    private double width;
-    private double heigh;
-    public Rectangle(){
 
-    }
-    public void area(){
-
-    }
-}
-
-public class J3 {
-    public static void main(String[] args) {
-        
-    }
-}
 interface IArea{
     void area();
 }
